f143e0b1591f9f4592b144b4ba4597a9cdc4b2f5
Frank <stylefk1218@gmail.com>
2025-03-16 12:00:33 +0800

    update_advanced_l2

--- apcs_advanced/L2/a471.py
+++ apcs_advanced/L2/a471.py
@@ -8,11 +8,11 @@ for i in sys.stdin:
 
     # 遍歷所有可能的起始數 i
     for i in range(0, N):  
-        sum = 0  # 初始化累加和
+        sum_ = 0  # 初始化累加和
         # 從 i 開始累加，檢查是否能得到 N
         for j in range(i, N):  
-            sum += j  # 累加當前數字
-            if sum == N:  # 如果總和剛好等於 N，表示找到解
+            sum_ += j  # 累加當前數字
+            if sum_ == N:  # 如果總和剛好等於 N，表示找到解
                 print(str(i) + "-" + str(j))  # 輸出這段連續數列的範圍
                 noSolution = False  # 設定為 False，表示至少找到一組解
                 break  # 跳出內層迴圈，開始找下一個起始數
