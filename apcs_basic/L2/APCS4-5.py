2556da99b47f208d8017973617d1341b83367d11
Frank <stylefk1218@gmail.com>
2025-07-26 16:46:24 +0800

    update_new_apcs

--- apcs_basic/L2/APCS4-5.py
+++
@@ -1,8 +0,0 @@
-score = int(input())  # 讀取使用者輸入的分數並轉換為整數
-
-if score >= 80:  # 若分數大於等於 80
-    print("A")  # 輸出 "A"
-elif 60 <= score <= 79:  # 若分數介於 60 到 79 之間（包含 60）
-    print("B")  # 輸出 "B"
-else:  # 若分數小於 60
-    print("C")  # 輸出 "C"
--- apcs_basic/L3/APCS7-3.py
+++
@@ -1,8 +0,0 @@
-#start
-
-N = int(input())  # 讀取輸入的數字 N
-
-# 產生一個列表，包含從 1 到 N，每個數字乘以 3 的結果
-num = [3 * x for x in range(1, N + 1)]
-
-print(num)  # 輸出結果列表
