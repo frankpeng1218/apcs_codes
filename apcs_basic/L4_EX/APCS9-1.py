2353586d0c53a2bfb73806baecf09f49cc89f5bd
Frank <stylefk1218@gmail.com>
2025-08-17 10:28:16 +0800

    update_l4

--- apcs_basic/L4_EX/APCS9-1.py
+++ apcs_basic/L4_EX/APCS9-1.py
@@ -1,13 +1,18 @@
-#start
+# start
+s = input()
+data = s.split()
 
-s = input()  # 讀取使用者輸入的字串
-data = s.split()  # 將輸入字串以空格分割成列表
+temp = int(data[0])
+weather = data[1]
 
-temp = int(data[0])  # 取得第一個數值並轉換為整數 (氣溫)
-weather = data[1]  # 取得第二個字串 (天氣狀況)
-
-# 使用舊式格式化方式輸出
+# old
 print("今天的氣溫是 %d 度, 天氣是 %s 天" % (temp, weather))
 
-# 使用 format() 方法格式化輸出
+# format
 print("今天的氣溫是 {} 度, 天氣是 {} 天".format(temp, weather))
+
+# f-string
+print(f"今天的氣溫是 {temp} 度, 天氣是 {weather} 天")
+
+# common
+print("今天的氣溫是 " + str(temp) + " 度," + " 天氣是 " + str(weather) + " 天")
